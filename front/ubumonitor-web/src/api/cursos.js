cbd89a2a3e4caee32255ae4253c1caeddb385f7e
Óscar Merino Martínez <117721876+oskitar04@users.noreply.github.com>
2026-05-27 12:05:24 +0200

    userId 27_05_26 12.05

--- front/ubumonitor-web/src/api/cursos.js
+++ front/ubumonitor-web/src/api/cursos.js
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:8080"; // Cambiar por puerto de backend
 
 
 
-export const getCursos = async (token, host) => {
+export const getCursos = async (token, host, userId) => { // userId para poder cogerlo una vez con el token obtenido
   const response = await axios.get(`${API_URL}/api/users/${userId}/courses`, {
     headers:{
       'X-Moodle-Token': token,
--- front/ubumonitor-web/src/api/login.js
+++ front/ubumonitor-web/src/api/login.js
@@ -6,6 +6,18 @@ const API_URL = "http://localhost:8080";
 
 export const login = async (host, username, password) => {
     const response = await axios.post(`${API_URL}/api/public/login/token`, { host, username, password });
-    return response.data; // De aquí es de donde cojo el token y el usuario (el usuario le paso a cursos)
+    return response.data; // Devuelve el token y el privatetoken
+};
+
+// Una vez con el token, obtiene info del sitio: userid, fullname, username...
+// El userId es necesario para llamar a /api/users/{userId}/courses
+export const getSiteInfo = async (token, host) => {
+    const response = await axios.get(`${API_URL}/api/site/info`, {
+        headers: {
+            'X-Moodle-Token': token,
+            'X-Moodle-Host': host
+        }
+    });
+    return response.data; // Devuelve userId a demás de otros datos
 };
 
