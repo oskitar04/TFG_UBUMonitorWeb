3878b9101f946c8ec254ed9f8a630a8dd66cb6b7
Óscar Merino Martínez <117721876+oskitar04@users.noreply.github.com>
2026-05-31 20:34:48 +0200

    31_05_26 20.34
    Borrado de trazas de prueba de error cors

--- front/ubumonitor-web/src/api/login.js
+++ front/ubumonitor-web/src/api/login.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Para conseguir el token al ahcer login
 
-const API_URL = "";
+const API_URL = "http://localhost:8080";
 
 
 export const login = async (host, username, password) => {
